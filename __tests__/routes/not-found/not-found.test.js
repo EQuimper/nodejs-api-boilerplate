c4c1527d78f9023cd42de4eead8c10d608fb8fbe
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2017-05-28 17:29:06 -0700

    adding tests for not-found path

---
+++ __tests__/routes/not-found/not-found.test.js
@@ -0,0 +1,54 @@
+import { expect } from 'chai';
+
+import server from '../../../__mocks__/utils/server.mock';
+
+const ENDPOINT = '/api/wrong/path';
+
+/**
+ * @test {not-found.test.js}
+ */
+describe(`ALL ${ENDPOINT}`, () => {
+  it('should return Not Found message when POST', done => {
+    server
+      .post(ENDPOINT)
+      .end((err, res) => {
+        const { body, status } = res;
+        expect(status).to.equal(404);
+        expect(body.message).to.equal("Not Found!");
+        done();
+      });
+  });
+
+  it('should return Not Found message when GET', done => {
+    server
+      .get(ENDPOINT)
+      .end((err, res) => {
+        const { body, status } = res;
+        expect(status).to.equal(404);
+        expect(body.message).to.equal("Not Found!");
+        done();
+      });
+  });
+
+  it('should return Not Found message when PUT', done => {
+    server
+      .put(ENDPOINT)
+      .end((err, res) => {
+        const { body, status } = res;
+        expect(status).to.equal(404);
+        expect(body.message).to.equal("Not Found!");
+        done();
+      });
+  });
+
+  it('should return Not Found message when DELETE', done => {
+    server
+      .delete(ENDPOINT)
+      .end((err, res) => {
+        const { body, status } = res;
+        expect(status).to.equal(404);
+        expect(body.message).to.equal("Not Found!");
+        done();
+      });
+  });
+});
